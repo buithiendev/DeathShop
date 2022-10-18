48ff8644aeb0a1fe65a866437d424b165ef9d32c
BuiThienDEV <buithien.dev@gmail.com>
2022-10-19 00:06:58 +0700

    update route api

--- dashboard/src/utils/APIRoutes.js
+++ dashboard/src/utils/APIRoutes.js
@@ -1,4 +1,7 @@
-const host = "http://localhost:5000"
-export const loginRoute = `${host}/api/auth/login`
+const host = 'http://localhost:5000';
+export const registerRoute = `${host}/api/auth/register`;
+export const checkEmail = `${host}/api/auth/checkEmail`;
+export const loginRoute = `${host}/api/auth/login`;
+export const getAllUsersRoute = `${host}/api/auth/getAllUsers`;
 
 // http://localhost:5000/api/auth/login
